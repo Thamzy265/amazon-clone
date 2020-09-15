5e063802c49d982b704f91b24afe8d9e7257d2af
thamzy265 <william5nasoni@gmail.com>
2020-09-15 10:33:35 +0200

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-	apiKey: "AIzaSyADUi0mgQI1VG-GG7BWNhGevNBhR_FNmAA",
-	authDomain: "clone-79fec.firebaseapp.com",
-	databaseURL: "https://clone-79fec.firebaseio.com",
-	projectId: "clone-79fec",
-	storageBucket: "clone-79fec.appspot.com",
-	messagingSenderId: "244800958776",
-	appId: "1:244800958776:web:c3990aa0e233ec3e4611cd",
-	measurementId: "G-JTMPZEVTZQ",
+	apiKey: "",
+	authDomain: "",
+	databaseURL: "",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: "",
+	appId: "",
+	measurementId: "",
 });
 
 const db = firebaseApp.firestore();
